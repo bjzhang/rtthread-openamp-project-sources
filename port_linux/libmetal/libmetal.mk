fe85739b30490884b65224fa02c2fd0a3835e295
Bamvor ZHANG <bamv2005@gmail.com>
2021-07-12 17:08:30 +0800

    add open-amp and libmetal buildroot makefile
    Signed-off-by: Bamvor ZHANG <bamv2005@gmail.com>

---
+++ port_linux/libmetal/libmetal.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# libmetal configuration. It make use of cmake.
+#
+################################################################################
+
+LIBMETAL_VERSION = devel
+LIBMETAL_SITE = ${BR2_EXTERNAL_OPENAMP_PATH}/port_linux/libmetal-src
+LIBMETAL_SITE_METHOD = local
+LIBMETAL_LICENSE = GPL-2.0+
+LIBMETAL_LICENSE_FILES = Copyright COPYING
+LIBMETAL_DEPENDENCIES = libsysfs
+LIBMETAL_CONF_OPTS = -DCMAKE_TOOLCHAIN_FILE=rv64_linux_cross_toolchain_file -DCMAKE_C_FLAGS="-ggdb" -DCMAKE_SYSROOT=$(shell $(TARGET_CC) --print-sysroot) -DWITH_EXAMPLES=ON
+
+$(eval $(cmake-package))
---
+++ port_linux/openamp/openamp.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# open-amp configuration. It make use of cmake.
+#
+################################################################################
+
+OPENAMP_VERSION = devel
+OPENAMP_SITE = ${BR2_EXTERNAL_OPENAMP_PATH}/port_linux/open-amp-src
+OPENAMP_SITE_METHOD = local
+OPENAMP_LICENSE = GPL-2.0+
+OPENAMP_LICENSE_FILES = Copyright COPYING
+OPENAMP_DEPENDENCIES = libsysfs
+OPENAMP_CONF_OPTS = -DCMAKE_TOOLCHAIN_FILE=rv64_linux_cross_toolchain_file -DCMAKE_C_FLAGS="-ggdb" -DCMAKE_SYSROOT=$(shell $(TARGET_CC) --print-sysroot) -DLIBMETAL_LIB_DIR=$(BASE_TARGET_DIR)/usr/lib -DLIBMETAL_LIB=$(BASE_TARGET_DIR)/usr/lib/libmetal.so -DLIBMETAL_INCLUDE_DIR=$(BASE_TARGET_DIR)/usr/include
+
+$(eval $(cmake-package))
