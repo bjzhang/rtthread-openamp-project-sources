c9441da8263cc5229c838b023c514bfcb0c2fef3
Bamvor ZHANG <bamv2005@gmail.com>
2021-07-19 13:33:15 +0800

    config: enable ssh root login
    Signed-off-by: Bamvor ZHANG <bamv2005@gmail.com>

--- buildroot/enable-ssh-login.sh
+++ buildroot/enable-ssh-login.sh
@@ -1,5 +1,6 @@
 #/bin/bash -x
 
 cd $1
-sed "s/^root::/root:\$1\$4f0YW3nK\$9psfLsrngu4mniQvJw5.N1:/g" etc/shadow -i
-#sed "s/#PermitRootLogin.*$/PermitRootLogin yes/g"  etc/ssh/sshd_config -i
+SSHD_CFG=etc/ssh/sshd_config
+echo "Match Address 10.0.2.*" >> $SSHD_CFG
+echo -e "\tPermitRootLogin yes" >> $SSHD_CFG
