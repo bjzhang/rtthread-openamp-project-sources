a7c4df1ac2ce99d9ae1e9c477257e3027ed0b1dd
Bamvor ZHANG <bamv2005@gmail.com>
2021-07-22 20:42:15 +0800

    script: move common script for generating patches
    Signed-off-by: Bamvor ZHANG <bamv2005@gmail.com>

--- scripts/generating_opensbi.sh
+++ scripts/generating_opensbi.sh
@@ -3,31 +3,19 @@
 set -x
 set -e
 
+PKG=opensbi
+PKG_UPSTREAM=234ed8e
+PKG_DIR=buildroot/opensbi
+PKG_SRC_DIR=buildroot/opensbi-src
+
 START_DIR=`pwd`
 SCRIPT_DIR=`dirname $0`
 SCRIPT_DIR=`realpath $SCRIPT_DIR`
 TOP_DIR=`dirname $SCRIPT_DIR`
+
 if [ "$TOP_DIR" = "" ]; then
 	echo FATAL: top dir should not empty.
 	exit
 fi
-OPENSBI_SRC_DIR=$TOP_DIR/buildroot/opensbi-src
-OPENSBI_DIR=$TOP_DIR/buildroot/opensbi
-if [ "$OPENSBI_DIR" = "" ]; then
-	echo "FATAL: opensbi directory $OPENSBI_DIR is empty"
-	exit
-fi
-if [ "$OPENSBI_SRC_DIR" = "" ]; then
-	echo "FATAL: opensbi directory $OPENSBI_SRC_DIR is empty"
-	exit
-fi
-cd $OPENSBI_SRC_DIR
-rm -f $OPENSBI_DIR/0*.patch
-echo "Output patches to opensbi dir"
-$SCRIPT_DIR/generating_patches.sh 234ed8e --output-directory $OPENSBI_DIR
-cd $OPENSBI_DIR
-echo "Create the new series"
-rm -f series
-ls --color=none 0*.patch > series
 
-cd $START_DIR
+$SCRIPT_DIR/generating_patches.sh $PKG $TOP_DIR/$PKG_SRC_DIR $TOP_DIR/$PKG_DIR $PKG_UPSTREAM
--- scripts/generating_patches.sh
+++ scripts/generating_patches.sh
@@ -3,6 +3,27 @@
 set -e
 set -x
 
-echo "This is part of script which will generate the release patches or source code tarball"
-echo "Usage $0 <git-format-patch-args>"
-git format-patch $*
+PKG=$1
+PKG_SRC_DIR=$2
+PKG_DIR=$3
+PKG_UPSTREAM=$4
+
+cd $START_DIR
+
+if [ "$PKG_DIR" = "" ]; then
+	echo "FATAL: $PKG directory $PKG_DIR is empty"
+	exit
+fi
+if [ "$PKG_SRC_DIR" = "" ]; then
+	echo "FATAL: $PKG directory $PKG_SRC_DIR is empty"
+	exit
+fi
+cd $PKG_SRC_DIR
+rm -f $PKG_DIR/0*.patch
+echo "Output patches to $PKG dir"
+git format-patch $PKG_UPSTREAM --output-directory $PKG_DIR
+cd $PKG_DIR
+echo "Create the new series"
+rm -f series
+ls --color=none 0*.patch > series
+
