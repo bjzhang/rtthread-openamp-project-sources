89c344eae69e9c64aa48165fbae3bc571180a933
Bamvor ZHANG <bamv2005@gmail.com>
2021-07-29 14:27:34 +0800

    Fixes: packages: libmetal header helper script
    Signed-off-by: Bamvor ZHANG <bamv2005@gmail.com>

--- scripts/libmetal-pre-configure.sh
+++ scripts/libmetal-pre-configure.sh
@@ -4,7 +4,8 @@ START_DIR=`pwd`
 SCRIPT_DIR=`dirname $0`
 SCRIPT_DIR=`realpath $SCRIPT_DIR`
 
-HEADERS="alloc.h  assert.h  atomic.h  cache.h  compiler.h  condition.h  cpu.h  device.h  dma.h  io.h  irq_controller.h  irq.h  mutex.h  shmem.h  sleep.h  softirq.h  spinlock.h  time.h  utilities.h  version.h"
+echo "Coping metal common headers"
+HEADERS="alloc.h assert.h atomic.h cache.h compiler.h condition.h cpu.h device.h dma.h io.h irq_controller.h irq.h list.h log.h mutex.h shmem.h sleep.h softirq.h spinlock.h sys.h time.h utilities.h version.h"
 PROJECT_SYSTEM=rtthread
 PROJECT_PROCESSOR=riscv
 HEADERS_LOCATION=lib
@@ -18,8 +19,10 @@ for f in $HEADERS; do
 	fi
 done
 
+echo "Creating rv64 libmetal config.h"
 cp -p $SCRIPT_DIR/libmetal_config.h $HEADERS_COMPILING_LOCATION/config.h
 
+echo "Coping compiler headers"
 GCC_HEADERS="compiler.h atomic.h"
 GCC_HEADERS_LOCATION=$HEADERS_LOCATION/compiler/gcc/
 GCC_HEADERS_COMPILING_LOCATION=$HEADERS_COMPILING_LOCATION/compiler/gcc
@@ -30,3 +33,15 @@ for f in $GCC_HEADERS; do
 	fi
 done
 
+echo "Coping processor headers"
+PROCESSOR_HEADERS="cpu.h"
+PROCESSOR_HEADERS_COMPILING_LOCATION=$HEADERS_COMPILING_LOCATION/processor/$PROJECT_PROCESSOR
+mkdir -p $PROCESSOR_HEADERS_COMPILING_LOCATION
+for f in $PROCESSOR_HEADERS; do
+	cp -p lib/processor/$PROJECT_PROCESSOR/$f $PROCESSOR_HEADERS_COMPILING_LOCATION
+done
+
+echo "Coping rt-thread headers"
+SYSTEM_HEADERS_COMPILING_LOCATION=$HEADERS_COMPILING_LOCATION/system/$PROJECT_SYSTEM
+mkdir -p $SYSTEM_HEADERS_COMPILING_LOCATION
+cp -p lib/system/$PROJECT_SYSTEM/*.h $SYSTEM_HEADERS_COMPILING_LOCATION
